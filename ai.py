bd89992e0f34d0d871b028d59afacceae45de844
Judah0715 <judah.agnew121@gmail.com>
2022-09-22 11:58:57 -0400

    ai.py

---
+++ ai.py
@@ -0,0 +1,16 @@
+import imp
+from player import Player
+import random
+
+class Ai(Player):
+    def __init__(self):
+        super().__init__()
+
+        def choose_gesture(self):
+            self.chosen_gesture = random.choice(self.gesture)
+            print(f"Machine chose {self.chosen_gesture}")
+            return self.chosen_gesture
+
+
+        def player_name(self):
+            self.name = "Machine"
