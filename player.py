93f4eefd12640966b06b9347e28fc34aaaeb2baf
Judah0715 <judah.agnew121@gmail.com>
2022-09-22 11:46:45 -0400

    player.py

---
+++ player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self):
+        self.gesture = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.name = "Machine"
+        self.player_score = 0
+
+    def choose_gesture():
+        pass     
